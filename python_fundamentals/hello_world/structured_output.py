fdd23a1bccc93b6b609268a3d4f2f6283ecc7ebd
Sultanjafari <Sultan06668@gmail.com>
2026-09-16 20:12:40 +0300

    Add structured output script

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 language = "Python"
 version = 3
 pi = 3.141592653589793
 valid = version == 3
-
 print(f"Language: {language}")
 print(f"Version: {version}")
 print(f"Pi approx: {pi:.2f}")
