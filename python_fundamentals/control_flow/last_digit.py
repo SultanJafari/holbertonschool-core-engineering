97eecea8c3c246d6f6b7d9500e26bf2cca3b9795
Sultanjafari <Sultan06668@gmail.com>
2026-09-17 12:00:57 +0300

    upd

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 
 number = __import__('random').randint(-10000, 10000)
 
 last_digit = number % 10 
-print(last_digit, number )
+#print(last_digit, number )
 
 if last_digit > 5:
-    print("last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit == 0:
-    print("last digit of", number, "is", last_digit, "and is 0")
+    print("Last digit of", number, "is", last_digit, "and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print("last digit of", number, "is", last_digit, "and is is greater than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is is less than 6 and not 0")
