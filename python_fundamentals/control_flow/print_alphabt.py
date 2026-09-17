f7e1f6399fc03e091ce5aefa795abf4f87113f0d
Sultanjafari <Sultan06668@gmail.com>
2026-09-17 19:35:21 +0300

    print_alqhabt

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 result = ""
-for i in range(ord("a"), ord("z")+ 1):
+for i in range(ord("a"), ord("z") + 1):
     c = chr(i)
     if c != 'e' and c != 'q':
         result += c
 
-print (result, end='')
+print(result, end='')
