b1aa57f8b81c3914a0276271bb5b6a18ac0a8b72
Sultanjafari <Sultan06668@gmail.com>
2026-09-17 12:42:34 +0300

    Save remaining file

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -4,8 +4,9 @@ language = "Python"
 version = 3
 pi = 3.141592653589793
 valid = version == 3
-
+name = "Sultan_Al Jafari"
 print(f"Language: {language}")
 print(f"Version: {version}")
-print(f"Pi approx: {pi:.2f}")
+print(f"Pi approx: {pi:.3f}")
 print(f"Computation valid: {valid}")
+print(f"Name :{name}")
