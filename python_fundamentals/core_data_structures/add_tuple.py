e74c318f394fd75d2045ba57b145d76aeae1394c
Sultanjafari <Sultan06668@gmail.com>
2026-09-19 22:47:16 +0300

    add

---
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
+
+
+if __name__ == "__main__":
+    print(add_tuple((1, 89), (88, 11)))
+    print(add_tuple((1, 89), (1,)))
+    print(add_tuple((1, 89), ()))
+    print(add_tuple((), ()))
