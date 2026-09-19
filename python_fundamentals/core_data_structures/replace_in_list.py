807d8d8e772c1c575ba7be7c3bf02b762000e43e
Sultanjafari <Sultan06668@gmail.com>
2026-09-19 22:39:14 +0300

    feat: add replace_in_list function

---
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print(replace_in_list(my_list, 3, 99))
+    print(replace_in_list(my_list, 15, 99))
