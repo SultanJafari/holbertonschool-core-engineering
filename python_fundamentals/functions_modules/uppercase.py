22370c7ac995be8167f7c90dd16bd333798b119e
Sultanjafari <Sultan06668@gmail.com>
2026-09-19 20:06:25 +0300

    Use str.format for print output compliance

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+
 def uppercase(text):
     new_str = ""
     for i in text:
@@ -6,4 +8,4 @@ def uppercase(text):
             new_str += chr(ord(i) - 32)
         else:
             new_str += i
-    print(new_str)
+    print("{}".format(new_str))
