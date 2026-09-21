e1d34b233cba4024ac7544d12db48e764c884c81
Sultanjafari <Sultan06668@gmail.com>
2026-09-21 10:47:25 +0300

    poth_list_int

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -8,7 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_printed += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
     return nb_printed
