b1f4902907c55b3f29eb4e3622884f06e8ca5b9c
Sultanjafari <Sultan06668@gmail.com>
2026-09-21 19:15:37 +0300

    size

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines a square."""
+Square = __import__('size').Square
+
+class Square:
+    """SUltan."""
+    def __init__(self, size):
+        self.__size = size
+    pass
