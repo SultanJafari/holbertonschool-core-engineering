0465cdb5972b2bb3a8ea5ac09f37af64d136f013
Sultanjafari <Sultan06668@gmail.com>
2026-09-22 20:20:12 +0300

    4sq

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -5,17 +5,24 @@
 class Square:
     """Represents a square."""
 
-    @property
     def __init__(self, size=0):
-        self.self = size
+        """Initialize the square with an optional size."""
+        self.size = size  # استدعاء الـ setter للتحقق من القيمة الأولية
 
-    def size(self, size=0):
+    @property
+    def size(self):
+        """Getter to retrieve the size."""
         return self.__size
+
     @size.setter
     def size(self, value):
-
-        if not isinstance(size, int):
+        """Setter to validate and set the size."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
+    def area(self):
+        """Returns the current area of the square."""
+        return self.__size * self.__size
