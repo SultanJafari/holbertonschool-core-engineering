30e0d59d85d962dc67a38c100f3476242cf155a0
Sultanjafari <Sultan06668@gmail.com>
2026-09-22 20:08:13 +0300

    *

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -6,8 +6,7 @@ class Square:
     """SUltan."""
 
     def __init__ (self, size=0):
+        self.__size = size
     def area (self):
         """Defines a square."""
         return self.__size * self.__size
-
-        self.__side = side
